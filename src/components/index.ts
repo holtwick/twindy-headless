f311ef971a93a3358650ccd563659b9eb837ad66
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-01-18 15:24:19 +0100

    Add popover code

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import twButton from "./tw-button.vue"
 import twModal from "./tw-modal.vue"
 import twItems from "./tw-items.vue"
 import twList from "./tw-list.vue"
+import twPopover from "./tw-popover.vue"
 
-export { twButton, twModal, twItems, twList }
+export { twButton, twModal, twItems, twList, twPopover }

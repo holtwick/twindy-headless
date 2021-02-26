80e59d45511b21b436af81caa19eeff27b01e25e
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-02-26 11:36:46 +0100

    Add types

---
+++ types/index.d.ts
@@ -0,0 +1,18 @@
+import twButton from "./tw-button.vue";
+import twModal from "./tw-modal.vue";
+import twItems from "./tw-items.vue";
+import twList from "./tw-list.vue";
+import twPopover from "./tw-popover.vue";
+import twCompletion from "./tw-completion.vue";
+import twTagInput from "./tw-tag-input.vue";
+import twInput from "./tw-input.vue";
+import twDropdown from "./tw-dropdown.vue";
+import twObject from "./tw-object.vue";
+import twIcon from "./tw-icon.vue";
+import twLabel from "./tw-label.vue";
+import twMenu from "./tw-menu.vue";
+import { useNotification } from "./notification";
+import { useTooltip } from "./tooltip";
+import { useTextareaResize } from "./usetextarea";
+import { useMenu } from "./menu";
+export { twButton, twModal, twItems, twList, twPopover, twCompletion, twTagInput, twInput, twDropdown, twObject, twIcon, twLabel, twMenu, useNotification, useTooltip, useTextareaResize, useMenu };

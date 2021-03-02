619916e065d66550ee4337bba6668e8b6d05adcc
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-03-02 15:59:53 +0100

    Types in dist

--- vite.config.ts
+++ vite.config.ts
@@ -10,14 +10,11 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "twindy-headless",
+      formats: ["es"],
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ["vue"],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: "Vue",
         },
@@ -27,13 +24,16 @@ export default defineConfig({
           check: true,
           clean: true,
           abortOnError: false,
-          useTsconfigDeclarationDir: true,
-          tsconfigOverride: {
-            compilerOptions: {
-              declaration: true,
-              declarationDir: "types",
-            },
-          },
+          // verbosity: 1,
+          // include: ["*.ts+(|x)", "**/*.ts+(|x)"],
+          // useTsconfigDeclarationDir: true,
+          // tsconfigOverride: {
+          //   compilerOptions: {
+          //     module: "ESNext",
+          //     declaration: true,
+          //     declarationDir: "types",
+          //   },
+          // },
         }),
       ],
     },

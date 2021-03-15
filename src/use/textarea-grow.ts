d63ecac018a30989fd78291af2294b8eb959d7f6
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-03-15 10:48:56 +0100

    Fix empty ref

--- src/use/textarea-grow.ts
+++ src/use/textarea-grow.ts
@@ -4,17 +4,20 @@ import { onUpdated, Ref } from "vue"
 export function useTextareaResize(r: Ref<HTMLTextAreaElement>): void {
   const resize = () => {
     let el = r.value
-    el.style.height = "1px"
-    el.style.height = el.scrollHeight + 2 + "px"
+    if (el) {
+      el.style.height = "1px"
+      el.style.height = el.scrollHeight + 2 + "px"
+    }
   }
 
   onUpdated(() => {
-    r.value.setAttribute(
-      "style",
-      "height:" +
-        (r.value.scrollHeight + 2) +
-        "px;overflow-y:hidden;resize:none;"
-    )
+    let el = r?.value
+    if (el) {
+      el.setAttribute(
+        "style",
+        "height:" + (el.scrollHeight + 2) + "px;overflow-y:hidden;resize:none;"
+      )
+    }
   })
 
   useEventListener(r, "input", resize)

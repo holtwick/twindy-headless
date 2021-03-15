ba475a4de3a86678ed474f5d2415c69f8645162c
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-03-15 10:01:28 +0100

    Apply a bug fix for the build process

--- dist/tw-icon.vue.d.ts
+++ dist/tw-icon.vue.d.ts
@@ -1,6 +1,4 @@
-declare const _default: import("vue").DefineComponent<{
-    name: StringConstructor;
-}, unknown, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{} & {
-    name?: string | undefined;
+declare const _default: import("vue").DefineComponent<unknown, object, {}, import("vue").ComputedOptions, import("vue").MethodOptions, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{
+    [x: string]: any;
 }>, {}>;
 export default _default;
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import typescript from "rollup-plugin-typescript2"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    dedupe: ["vue"],
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),

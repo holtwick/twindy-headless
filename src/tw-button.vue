3c64cf190178809e718d86dfb94387a0d5ff3760
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-03-26 18:01:14 +0100

    No slots, no spans

--- src/tw-button.vue
+++ src/tw-button.vue
@@ -1,14 +1,14 @@
 <template>
   <button
     class="tw-button"
-    @click="$emit('action')"
+    @click="$emit('action'), $emit('click', $event)"
     v-bind="$attrs"
     :disabled="disabled"
   >
-    <span v-if="left"><slot /></span>
+    <span v-if="left && $slots.default"><slot /></span>
     <tw-icon v-if="name || icon" :name="name || icon" />
     <tw-symbol v-if="symbol" :name="symbol" />
-    <span v-if="!left"><slot /></span>
+    <span v-if="!left && $slots.default"><slot /></span>
   </button>
 </template>
 
@@ -17,12 +17,21 @@ import { defineComponent } from "vue"
 import twIcon from "./tw-icon.vue"
 import twSymbol from "./tw-symbol.vue"
 
+/**
+ * Features:
+ *
+ * - Supports `icon` (svg) and `symbol` (fonts)
+ * - Image can go `left` or `right`
+ * - Title can be passed via slot
+ * - `@action` to have simple TS compatible event listeners
+ */
+
 export default defineComponent({
   components: {
     twIcon,
     twSymbol,
   },
-  emits: ["action"],
+  emits: ["action", "click"],
   props: {
     name: String,
     icon: String,

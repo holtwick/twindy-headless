146e2e75b8d643f118912f491016e36e61d178da
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-04-02 13:32:46 +0200

    Smarter key shortcut handling having just one listener

--- src/use/key.ts
+++ src/use/key.ts
@@ -1,28 +1,61 @@
 import { onBeforeUnmount, onMounted } from "vue"
 
+interface KeyListener {
+  key: string
+  ignoreInput: boolean
+  handler: Function
+}
+
+let listeners: KeyListener[] = []
+
+let handler = (ev: KeyboardEvent) => {
+  for (let { key, ignoreInput, handler } of listeners) {
+    // @ts-ignore
+    let tagName = ev.target.tagName
+    if (ignoreInput && ["INPUT", "TEXTAREA"].includes(tagName)) {
+      continue
+    }
+    // console.log("key match", key)
+    if (ev.key === key) {
+      handler(ev)
+      ev.preventDefault()
+      ev.stopPropagation()
+    }
+  }
+}
+
+function addListener(listener: KeyListener) {
+  if (listeners.length <= 0) {
+    window.addEventListener("keydown", handler)
+  }
+  listeners.push(listener)
+}
+
+function removeListener(listener: KeyListener) {
+  let index = listeners.indexOf(listener)
+  if (index >= 0) {
+    listeners.splice(index, 1)
+  }
+  if (listeners.length <= 0) {
+    window.removeEventListener("keydown", handler)
+  }
+}
+
 export function useKey(
   key: string,
   fn: EventHandlerNonNull,
   opt: { ignoreInputElements: boolean } = { ignoreInputElements: true }
 ) {
-  let handler = (ev: KeyboardEvent) => {
-    // console.log("key", key, opt?.ignoreInputElements, ev.key)
-    // @ts-ignore
-    let tagName = ev.target.tagName
-    // console.log("key match", key)
-    if (ev.key === key) {
-      const ignoreInput = opt?.ignoreInputElements === true
-      if (ignoreInput !== true || !["INPUT", "TEXTAREA"].includes(tagName)) {
-        fn(ev)
-      }
-      ev.preventDefault()
-      ev.stopPropagation()
-    }
+  const handler: KeyListener = {
+    key,
+    ignoreInput: opt?.ignoreInputElements === true,
+    handler: fn,
   }
+
   onMounted(() => {
-    window.addEventListener("keydown", handler)
+    addListener(handler)
   })
   onBeforeUnmount(() => {
-    window.removeEventListener("keydown", handler)
+    removeListener(handler)
   })
 }

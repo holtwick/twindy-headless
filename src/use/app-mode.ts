9b198e5313294a5aecf6266aa3f2aca71b0a6b54
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-04-03 16:40:38 +0200

    useAppMode

---
+++ src/use/app-mode.ts
@@ -0,0 +1,21 @@
+import { onBeforeMount, onBeforeUnmount } from "vue"
+
+export function useAppMode() {
+  let oldOverflow: string
+  let oldHeight: string
+
+  onBeforeMount(() => {
+    oldOverflow = document.body.style.overflow
+    oldHeight = document.body.style.height
+
+    // const appStyle = "-webkit-overflow: hidden; overflow: hidden; height: 100%"
+    // document.documentElement.setAttribute("style", appStyle)
+    // document.body.setAttribute("style", appStyle)
+    document.body.style.overflow = "hidden"
+    document.body.style.height = "100%"
+  })
+  onBeforeUnmount(() => {
+    document.body.style.overflow = oldOverflow
+    document.body.style.height = oldHeight
+  })
+}
